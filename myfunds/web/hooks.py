f74276c63c2c419e1c8a49eb58ed375a89114502
anzodev <anzodev@no.email>
2021-03-23 01:10:05 +0200

    add 404 error handling

--- myfunds/web/hooks.py
+++ myfunds/web/hooks.py
@@ -9,5 +9,23 @@ def _log_request():
     logger.info(f"{req.remote_addr} - {req.method} {req.full_path}")
 
 
+def _errorhandler_404(e: Exception):
+    logger = logging.getLogger("myfunds.web")
+    error_msg = repr(e)
+    status_code = e.code
+    description = e.description
+    return_url = flask.session.get("last_page")
+
+    logger.error(error_msg)
+
+    return flask.render_template(
+        "pages/error.html",
+        status_code=status_code,
+        description=description,
+        return_url=return_url,
+    )
+
+
 def init_app(app: flask.Flask) -> None:
     app.before_request(_log_request)
+    app.errorhandler(404)(_errorhandler_404)
