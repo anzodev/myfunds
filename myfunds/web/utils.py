2f93e51a90bce35b07b6837ae68149f54fb2736d
anzodev <3552206-anzodev@users.noreply.gitlab.com>
2021-09-12 16:52:45 +0300

    add new utility functions

--- myfunds/web/utils.py
+++ myfunds/web/utils.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from argparse import Namespace
+from datetime import datetime
 
 
 def env_parser() -> ArgumentParser:
@@ -13,3 +14,28 @@ def env_parser() -> ArgumentParser:
 def parse_env_parser() -> Namespace:
     parser = env_parser()
     return parser.parse_args()
+
+
+def make_amount_pattern(currency_precision: int) -> str:
+    pattern = r"^[1-9]{1}\d*"
+    if currency_precision == 0:
+        return f"{pattern}$"
+
+    fract_len = "{1}" if currency_precision == 1 else f"{{1,{currency_precision}}}"
+    return f"{pattern}(\\.\\d{fract_len})?$"
+
+
+def make_amount_placeholder(currency_precision: int) -> str:
+    placeholder = "100"
+    if currency_precision == 0:
+        return placeholder
+    return ".".join([placeholder, "0" * currency_precision])
+
+
+def amount_to_subunits(amount_repr: str, currency_precision: int) -> int:
+    return round(float(amount_repr) * (10 ** currency_precision))
+
+
+def datetime_range_from_first_month_day_to_now() -> tuple[datetime, datetime]:
+    now = datetime.now()
+    return (now.replace(day=1, hour=0, minute=0, second=0, microsecond=0), now)
---
+++ tests/test_web/test_utils.py
@@ -0,0 +1,41 @@
+import pytest
+
+from myfunds.web import utils
+
+
+@pytest.mark.parametrize(
+    "currency_precision,result",
+    [
+        [0, r"^[1-9]{1}\d*$"],
+        [1, r"^[1-9]{1}\d*(\.\d{1})?$"],
+        [2, r"^[1-9]{1}\d*(\.\d{1,2})?$"],
+        [3, r"^[1-9]{1}\d*(\.\d{1,3})?$"],
+    ],
+)
+def test_make_amount_pattern(currency_precision, result):
+    assert utils.make_amount_pattern(currency_precision) == result
+
+
+@pytest.mark.parametrize(
+    "currency_precision,result",
+    [
+        [0, "100"],
+        [1, "100.0"],
+        [2, "100.00"],
+        [3, "100.000"],
+    ],
+)
+def test_make_amount_placeholder(currency_precision, result):
+    assert utils.make_amount_placeholder(currency_precision) == result
+
+
+@pytest.mark.parametrize(
+    "args,result",
+    [
+        [("100", 0), 100],
+        [("100.5", 1), 1005],
+        [("100.25", 2), 10025],
+    ],
+)
+def test_amount_to_subunits(args, result):
+    assert utils.amount_to_subunits(*args) == result
